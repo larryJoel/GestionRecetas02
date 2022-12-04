8435d5afe1a4fbb778592dc31cfefad7684c2871
Larry González <larry.g.m1024@gmail.com>
2022-12-04 19:48:35 -0300

    my first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GestionRecetas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
